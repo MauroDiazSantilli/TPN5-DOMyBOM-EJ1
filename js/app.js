f5b976b6aee3b4a11d73d10b0edfa8ecfa6bd5f8
MauroDS <mauro.diaz.santilli@gmail.com>
2023-03-29 12:16:01 -0300

    Actualizando ejercicio1

--- js/app.js
+++ js/app.js
@@ -1,10 +1,7 @@
-let botnComenz = document.getElementById("botnComenz")
 let containJuego = document.getElementById("containJuego")
 let adivinarNumero = document.getElementById("adivinarNumero")
-let adivinarBoton = document.getElementById("adivinarBoton")
 let numeroUsuario
 
-botnComenz.addEventListener("click", Comenzar)
 
 function Comenzar(){
     numeroUsuario = Math.floor(Math.random()* 100) + 1
@@ -12,15 +9,13 @@ function Comenzar(){
     botnComenz.classList.add("d-none")
 }
 
-adivinarBoton.addEventListener("click", Chequear)
-
 function Chequear(){
-    let Interaccion = parseInt(adivinarNumero.value)
+    let interaccionUsuario = parseInt(adivinarNumero.value)
 
-    if(Interaccion === numeroUsuario){
+    if(interaccionUsuario === numeroUsuario){
         alert("Felicidades, ha adivinado el numero magico correctamente")
         location.reload()
-    } else if(Interaccion > numeroUsuario){
+    } else if(interaccionUsuario > numeroUsuario){
         alert("El numero ingresado es mayor que el numero magico")
     } else{
         alert("El numero ingresado es menor que el numero magico")
